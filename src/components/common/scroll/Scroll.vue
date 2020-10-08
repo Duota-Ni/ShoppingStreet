384540b84fa499e5c38ed8496f0a98259e7a1131
Duota <duota001@163.com>
2020-10-08 21:53:58 +0800

    scroll backtop

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -11,15 +11,58 @@ import BScroll from 'better-scroll';
 
 export default {
   name:'Scroll',
+  props:{
+    probeType:{
+      type:Number,
+      default:0
+    },
+    pullUpLoad:{
+      type:Boolean,
+      default:false
+    }
+  },
   data(){
     return {
       scroll:null
     }
   },
   mounted() {
+    if(!this.$refs.wrapper) return 
     this.scroll = new BScroll(this.$refs.wrapper,{
-      
+      probeType:this.probeType,
+      click:true,
+      pullUpLoad:this.pullUpLoad
     })
+    if(this.probeType === 2 || this.probeType === 3){
+      this.scroll.on('scroll',(position) => {
+        this.$emit('scroll',position)
+      })
+    }
+    if(this.pullUpLoad){
+      this.scroll.on('pullingUp',()=>{
+        this.$emit('pullingUp')
+      })
+    }
+  },
+  methods:{
+    scrollTo(x,y,time=300){
+      this.scroll && this.scroll.scrollTo(x,y,time);
+      console.log('backTop跳转');
+    },
+    finishPullUp(){
+      this.scroll.finishPullUp();
+    },
+    refresh(){
+      this.scroll && this.scroll.refresh();
+    },
+    getScrollY(){
+      return this.scroll ? this.scroll.y : 0;
+    }
+  },
+  watch:{
+    data(){
+      setTimeout(this.refresh,20)
+    }
   }
 }
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,14 +3,33 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <scroll class="content">
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+      ref="tabControl1"
+      v-show="isTabFixed"
+    ></tab-control>
+    <scroll
+      class="content"
+      ref="scroll"
+      :probe-type="3"
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp="loadMore"
+    >
       <home-swiper :banners="banners" />
       <recommend-view :recommends="recommends" />
       <feature-view />
-      <tab-control :titles="['流行','新款','精选']" @tabClick="tabClick" />
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        @tabClick="tabClick"
+        ref="tabControl2"
+      />
       <goods-list :goods="showGoods" />
     </scroll>
-    <back-top />
+    <!--当我们需要监听一个组件的原生事件时，必须给对应的事件加上.native修饰符-->
+    <back-top @click.native="backTop" v-show="isShowBackTop" />
   </div>
 </template>
 
@@ -19,13 +38,15 @@ import NavBar from "components/common/navbar/NavBar";
 import Scroll from "components/common/scroll/Scroll";
 import TabControl from "components/content/tabControl/TabControl";
 import GoodsList from "components/content/goods/GoodsList";
-import BackTop from "components/content/backTop/BackTop";
 
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
 import FeatureView from "./childComps/FeatureView";
 
 import { getHomeMultidata, getHomeGoods } from "network/home";
+import { debounce } from "common/utils";
+import { itemListenerMixin, backTopMixin } from "common/minxin";
+import { BACK_POSITION } from "common/const";
 
 export default {
   name: "Home",
@@ -34,7 +55,7 @@ export default {
     TabControl,
     GoodsList,
     Scroll,
-    BackTop,
+    // BackTop,
 
     HomeSwiper,
     RecommendView,
@@ -50,8 +71,13 @@ export default {
         sell: { page: 0, list: [] },
       },
       currenttype: "pop",
+
+      tabOffsetTop: 0,
+      isTabFixed: false,
+      saveY: 0,
     };
   },
+  mixins: [itemListenerMixin, backTopMixin],
   created() {
     //1.请求多个数据
     this.getHomeMultidata();
@@ -61,6 +87,14 @@ export default {
     this.getHomeGoods("new");
     this.getHomeGoods("sell");
   },
+  activated() {
+    this.$refs.scroll.scrollTo(0, this.saveY, 0);
+    this.$refs.scroll.refresh();
+  },
+  deactivated() {
+    this.saveY = this.$refs.scroll.getScrollY();
+    this.$bus.$off("itemImgLoad", this.itemImgListener);
+  },
   computed: {
     showGoods() {
       return this.goods[this.currenttype].list;
@@ -68,6 +102,10 @@ export default {
   },
   methods: {
     //事件监听相关的方法
+    loadMore() {
+      this.getHomeGoods(this.currenttype);
+      this.$refs.scroll.refresh();
+    },
     tabClick(index) {
       switch (index) {
         case 0:
@@ -80,6 +118,23 @@ export default {
           this.currenttype = "sell";
           break;
       }
+      this.$refs.tabControl1.currentIndex = index;
+      this.$refs.tabControl2.currentIndex = index;
+    },
+    // backTop(){
+    //   this.$refs.scroll.scrollTo(0,0)
+    //   console.log("homeback");
+    // },
+    contentScroll(position) {
+      //1.判断BackTop是否显示
+      this.listenShowBackTop(position);
+      //2.决定tabControl是否吸顶
+      this.isTabFixed = -position.y > this.tabOffsetTop;
+    },
+    swiperImageLoad() {
+      //获取tabControl的所有offsetTop,所有组件都有$el属性，用于获取组件中的元素
+      this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop;
+      console.log(this.tabOffsetTop);
     },
 
     //网络请求相关的方法
@@ -94,6 +149,9 @@ export default {
       getHomeGoods(type, page).then((res) => {
         this.goods[type].list.push(...res.data.list);
         this.goods[type].page += 1;
+
+        //完成上拉
+        this.$refs.scroll.finishPullUp();
       });
     },
   },
